479732c7037fa0d3a0cf7a03336c609d945bb415
sist-106 <sist-106@DESKTOP-645C4ON>
2024-11-18 14:19:23 +0900

    first

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,67 @@
+/*
+ 1. 대문자 구분 
+ 2. 들여 쓰기 => 가독성
+ 3. 명령문 종료 반드시 ;
+ 파이썬/코틀린/jvs ; 사용X
+ 4. 자바는 {
+ 5. 문법
+ 	데이터저장 ->데이터 처리(요청) -> 화면 출력
+ 	 |				|			  |
+ 	 			연산자/제어문		명령프롬포트: Application
+ 	 			=========		브라우저 출력: Web  Application
+ 	 				|			모바일 출력 : Mobile Application
+ 	 			메소드			spring : Java / Kotlin         
+ 	 			
+ 	한 개 저장 (메모리) :(변수):(상수)
+ 	여러개 저장 : 배열
+ 				클래스
+ 	------------------
+ 	프로그램 종료시에 사라짐
+ 	파일/오라클 -> 영구적인 저장
+ 	
+ 	=====>
+ 	
+ 	1. 기능을 모아서 관리
+ 		===
+ 		 =>단락 : 메소드
+ 		 =>클래스 : 객체 => 객체지향 프로그램
+ 		 =>프로그램 유지(예외처리)
+ 		 =>라이브러리 : 자바에 지원
+ 		 =>1차 응용프로그램
+ 		 
+ 	=====> web
+ 	1. HTML : 브라우저에서 실행
+ 				자바는 브라우저에서는 일반 텍스트
+ 		=> 투박 =>   CSS /  자바스크립트
+ 	2/ JSP (요청 > HTML > Java)
+ 			응답 > Java > HTML
+ 		MVC
+ 		===
+ 		Front / Back
+ 		둘다가 풀스택
+ 		
+ 	3. 통합 : Framework : Spring
+ 	 
+ 	4. 데이터 분석 : 파이썬, 넘파이,판다스
+ 	
+ 	5. NoSql => 일라스틱sql
+ 	
+ 	6. 배포 : AWS
+ 	=============
+ 	Spring-Boot jpa/mysql/redux
+ 	
+ 	자바/오라클/알고리즘
+ 	HTML/CSS/javaScript/JSP/Spring-boot/Python/데이터 분석
+ 	elasticSearch              
+ 	
+ 	
+ */
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 System.out.println("hello");
+			   
+	}
+
+}
