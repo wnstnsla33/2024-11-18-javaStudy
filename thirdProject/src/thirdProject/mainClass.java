710b4f7ad99fc50068c07c762ae910907d4fd419
sist-106 <sist-106@DESKTOP-645C4ON>
2024-11-18 15:18:24 +0900

    자바파일 열기

---
+++ thirdProject/src/thirdProject/mainClass.java
@@ -0,0 +1,24 @@
+package thirdProject;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		// javac java파일 입력시 class파일 생김
+		// java (class파일) 입력시 해당 자바파일 실행됨
+		
+		/*
+		 자바 실행
+		 ======
+		 컴파일
+		 => 파일 => 기계언어로 변경 (컴퓨터가 인식하는 언어)
+		 	 Java /c#/C/C++
+		 인터프리터 : 한줄씩 읽어서 출력
+		 	HTML / Java...
+		 	
+		  
+		 */
+	}
+
+}
--- thirdProject/src/thirdProject/세번째자바.java
+++ thirdProject/src/thirdProject/세번째자바.java
@@ -3,5 +3,10 @@ package thirdProject;
 public class 세번째자바 {
 	public static void main(String[] args) {
 		System.out.println("3번쨰");
+		
+		
 	}
 }
+
+
+
