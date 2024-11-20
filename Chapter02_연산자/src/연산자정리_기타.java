806a9330e922d3e5f30fdd8dd04651a30370af10
sist-106 <sist-106@DESKTOP-645C4ON>
2024-11-20 17:47:09 +0900

    연산자정리 기타

---
+++ Chapter02_연산자/src/연산자정리_기타.java
@@ -0,0 +1,20 @@
+
+public class 연산자정리_기타 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 특수문자
+		 * \n >다음줄로
+		 * \t >탭한번
+		 * \" >" 출력하고 싶을때
+		 * \\ > \를 출력할때
+		 * 
+		 * System.out.println(); == System.out.print("\n");
+		 */
+		System.out.println("hello\tHello\tHello");
+		System.out.println("\"홍길동\"");
+		
+	}
+
+}
