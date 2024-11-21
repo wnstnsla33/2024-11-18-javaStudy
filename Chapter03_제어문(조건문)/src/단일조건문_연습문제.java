75bdba107f9fa66c02c96d45e12ddada52d4ca97
sist-106 <sist-106@DESKTOP-645C4ON>
2024-11-21 17:47:56 +0900

    단일 조건문 문제풀이

--- Chapter03_제어문(조건문)/src/단일조건문_연습문제.java
+++ Chapter03_제어문(조건문)/src/단일조건문_연습문제.java
@@ -20,7 +20,10 @@ public class 단일조건문_연습문제 {
 		else if(season>=6&&season<=8)System.out.println("여름");
 		else if(season>=9&&season<=11)System.out.println("가을");
 		else if(season>=12&&season<=2)System.out.println("겨울");
+		
 		else System.out.println("잘못입력하였습니다");
 		
+			
 	}
+	
 }
